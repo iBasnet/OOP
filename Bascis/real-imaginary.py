58ede1cf266093f18a3501995c1b498d10ec3315
Pratik Basnet <99023466+iBasnet@users.noreply.github.com>
2023-03-08 12:23:06 +0545

    Add files via upload

---
+++ Bascis/real-imaginary.py
@@ -0,0 +1,19 @@
+# Object Oriented Programming in Python
+class complex:
+
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def add(self, number):
+        real = self.real + number.real
+        imag = self.imag + number.imag
+        result = complex(real, imag)
+        return result
+
+
+n1 = complex(2, 4)
+n2 = complex(1, 1)
+result = n1.add(n2)
+print("real =", result.real)
+print("imag =", result.imag)
